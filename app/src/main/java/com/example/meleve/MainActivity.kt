b28d412be509ec3fd2f9fdbe2e5a9a80a64d8dfd
Josileudo Rodrigues <josileudorodrigues75@gmail.com>
2024-12-10 06:59:19 -0300

    :sparkles: feat: Google maps configured

---
+++ app/src/main/java/com/example/meleve/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.meleve
+
+import android.R
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.meleve.ui.theme.MeLeveTheme
+import com.google.maps.android.compose.GoogleMap
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            setTheme(R.style.Theme)
+            MeLeveTheme(dynamicColor = false) {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    GoogleMap(
+                        modifier = Modifier.fillMaxSize(),
+                    )
+                }
+            }
+        }
+    }
+}
